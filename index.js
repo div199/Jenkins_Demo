6d22c4fe14cedd81c1c5f29f6f417ca01eb1d689
Dasgupta <agdasgupta@teksystems.com>
2019-04-18 13:52:42 +0530

    .

---
+++ index.js
@@ -0,0 +1,47 @@
+const http = require('http')
+const express = require('express')
+
+const app=express()
+
+
+
+app.get('/test',(request,response)=>{
+
+	response.send('Hellow World')
+})
+
+app.get('/calculate/:num1/:num2/:oper',(request,response)=>{
+
+
+    var x=calculate(Number(request.params.num1),Number(request.params.num2),request.params.oper)
+    console.log(request.params)
+    response.send(x.toString())
+	//response.send(request.params)
+
+
+
+})
+
+
+function calculate(num1,num2,oper) {
+   
+	if(oper=='add')
+		return num1+num2;
+    else if(oper=='sub')
+		return num1-num2;
+	else if(oper=='mod')
+		return num1%num2;
+	else if(oper=='mul')
+		return num1*num2;
+	else if(oper=='div')
+		return num1/num2;
+	else
+		return -1;
+
+	
+} 
+
+app.listen(3000,()=>{
+	console.log('Application listening at port 3000')
+})
+
