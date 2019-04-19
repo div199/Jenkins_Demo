48ba51d7e513f9107aef2f9f8071dd3b08a8fdb5
Dasgupta <agdasgupta@teksystems.com>
2019-04-19 14:05:52 +0530

    index.js

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 const http = require('http')
 const express = require('express')
-//const bodyParser=require('body-parser')
-//app.use(express.bodyParser());
-
+const bodyParser=require('body-parser')
 const app=express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
-app.post('/test1',function(request,response){
-    
+
+
+
+
+
+
+app.post('/test',(request,response)=>{
+
     console.log(request);
-	response.send('Hellow World');
+        response.send('Hellow World');
 })
 
 app.get('/calculate/:num1/:num2/:oper',(request,response)=>{
 
 
-   var x=calculate(Number(request.params.num1),Number(request.params.num2),request.params.oper)
+    var x=calculate(Number(request.params.num1),Number(request.params.num2),request.params.oper)
+    console.log(x)
     response.send(x.toString())
-	//response.send(request.params)
-}) 
+        //response.send(request.params)
 
-function calculate(num1,num2,oper) {
-   
-	if(oper=='add')
-		return num1+num2*5;
-    else if(oper=='sub')
-		return num1-num2;
-	else if(oper=='mod')
-		return num1%num2;
-	else if(oper=='mul')
-		return num1*num2;
-	else if(oper=='div')
-		return num1/num2;
-	else
-		return -1;
 
-	
-} 
 
-app.listen(4500,function(){
-	console.log('Application listening at port 4500');
 })
 
+
+function calculate(num1,num2,oper) {
+
+        if(oper=='add')
+                return num1+num2*5;
+    else if(oper=='sub')
+                return num1-num2;
+        else if(oper=='mod')
+                return num1%num2;
+        else if(oper=='mul')
+                return num1*num2;
+        else if(oper=='div')
+                return num1/num2;
+        else
+                return -1;
+
+
+}
+
+app.listen(3000,()=>{
+        console.log('Application listening at port 3000');
+})
